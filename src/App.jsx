7aec193bc381d7d68e92901f757fae2eb18fdf57
Vasanth RS <vasanth.rs@proton.me>
2025-12-22 20:29:36 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
+function App() {
+  return (
+    <mail>
+      <div className="text-red-600">GTA 6</div>
+    </mail>
+  );
+}
+
+export default App;
